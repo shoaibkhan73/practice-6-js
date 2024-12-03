e67070d19046545e611589605ebfe81060947cb3
shoaibkhan73 <khan973973@gmail.com>
2024-12-03 19:32:45 +0500

    practice no 6 is comleted

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("this is practice no 6 ");
+
+let a = "Shoaib";
+console.log("my name is ", a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// here is console.log( a[6] ); undefined because we don't have 6 number char 
+console.log(a[6]);
+
+
+let b = "amri";
+console.log(b);
+
+let c = "jawad";
+console.log(c);
+
+console.log(c.length);
+console.log(c.concat(" says that amir and shoaib", "is my elder brother"));
+console.log(c.includes("ja"));
+console.log(c.indexOf("a"));
+console.log(c.lastIndexOf("d"));
+console.log(c.toUpperCase());
+// you can use more also with varable name wit . like  console.log(c.); c is varable  after . use ctrl+space button then you can see more function 
